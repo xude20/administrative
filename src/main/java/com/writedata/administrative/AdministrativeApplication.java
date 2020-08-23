ed4b769669301c1b034353964ecbde1ee98fce0c
xude <980234608@qq.com>
2020-08-23 20:31:04 +0800

    create git

---
+++ src/main/java/com/writedata/administrative/AdministrativeApplication.java
@@ -0,0 +1,13 @@
+package com.writedata.administrative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdministrativeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/writedata/administrative/AdministrativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writedata.administrative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
